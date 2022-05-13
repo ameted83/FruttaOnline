72e18c210927d0578c8979deabc75b34524fed69
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-13 17:30:04 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Store from "./pages/Store";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
