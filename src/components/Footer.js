a1c3ce1d9812c3454fceb97a951eea1c10743bbb
Enrico De Maria <enricodem35@gmail.com>
2022-05-13 17:49:04 +0200

    feature/footer

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="bg-green-500 h-[150px] w-screen fixed bottom-0 text-white">
+        <div className="flex flex-wrap justify-around items-center mt-8">
+          <div>
+            <ul>
+              <li>Chi siamo</li>
+              <li>Contatti</li>
+              <li>News</li>
+              <li>P. iva</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Chi siamo</li>
+              <li>Contatti</li>
+              <li>News</li>
+              <li>P. iva</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Chi siamo</li>
+              <li>Contatti</li>
+              <li>News</li>
+              <li>P. iva</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Store = () => {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 };
