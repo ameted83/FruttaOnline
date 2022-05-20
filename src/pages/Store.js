a039f486645f073f2f9de17ae5cbaab7a6c2cee4
Enriko35 <enricodem35@gmail.com>
2022-05-20 15:51:38 +0200

    feature/card

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Product from "../components/Product"
+import Product from "../components/Product";
 
 const Store = () => {
   return (
     <div>
       <Navbar />
-      <Product/>
+      <Product />
       <Footer />
     </div>
   );
