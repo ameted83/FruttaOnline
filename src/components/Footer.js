07f3d90f846f7621cfa145760558d68f3843f2cf
Enriko35 <enricodem35@gmail.com>
2022-05-24 12:34:43 +0200

    feature/footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { developers } from "../Developers";
+import DeveloperPopUp from "./DeveloperPopUp";
 
 const Footer = () => {
+  const [devPopup, setdevPopup] = useState(false);
+
+  const handleDevPopUp = () => setdevPopup(!devPopup);
+  const [devData, setDevData] = useState(null);
+
   return (
     <div>
-      <div className="bg-green-500 h-[150px] w-screen fixed bottom-0 text-white">
+      <div className="bg-green-500 h-[250px] w-screen fixed bottom-0 text-white">
         <div className="flex flex-wrap justify-around items-center mt-8">
           <div>
             <ul>
@@ -23,10 +30,21 @@ const Footer = () => {
           </div>
           <div>
             <ul>
-              <li>Chi siamo</li>
-              <li>Contatti</li>
-              <li>News</li>
-              <li>P. iva</li>
+              <h2 className="font-bold text-xl">Sviluppato da:</h2>
+              {developers.map((developer) => {
+                return (
+                  <li
+                    className="cursor-pointer hover:underline"
+                    onClick={() => {
+                      setDevData(developer);
+                      handleDevPopUp();
+                    }}
+                  >
+                    {developer.name}
+                  </li>
+                );
+              })}
+              {devPopup && <DeveloperPopUp dev={devData} state={setdevPopup} />}
             </ul>
           </div>
         </div>
