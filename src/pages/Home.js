50a28b4e964181ae8657c0cf51d2891ab2924cf0
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-25 19:32:59 +0200

    demo.0.3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Product from "../components/Product";
 
 import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
+      <Product />
       <Footer />
     </div>
   );
