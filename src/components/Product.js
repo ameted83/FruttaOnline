2816cc6389a0f2a59a6d51190f12a08e479dc36b
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-06-02 12:13:08 +0200

    fix-card

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,18 +17,23 @@ const Product = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="max-w-[1100px] mx-auto py-8 w-full my-20 ">
-      <div className="mb-4 flex justify-center items-center">
+    <>
+      <div className="mt-[50px] flex justify-center items-center">
         <input
-          className=" rounded-lg p-2 w-[90%] border-0 mt-2 mb-4 outline-0 text-lg bg-slate-100"
+          className=" rounded-lg p-3 w-[90%] border-0 mt-2 mb-4 outline-0 text-lg bg-slate-100"
           onChange={(event) => setSearch(event.target.value)}
           type="search"
           id="search"
           placeholder="Cerca il tuo frutto..."
         ></input>
       </div>
-      <div className="relative grid mx-auto justify-center items-center w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        {loading && <h1>Caricamento prodotti in corso...</h1>}
+      <div className="flex flex-wrap gap-4 mx-auto p-20 font-sans">
+        {loading ? (
+          <div className="text-3xl font-bold">Caricamento dati in corso...</div>
+        ) : (
+          false
+        )}
+
         {product
           .filter((fruit) => {
             if (search === "") {
@@ -39,61 +44,44 @@ const Product = () => {
               return fruit;
             }
           })
-          .map((fruit, index) => (
-            <div
-              key={index}
-              className="flex flex-col mx-auto justify-center items-center w-[250px] "
-            >
-              <div>
-                <h1 className="font-bold text-2xl">{fruit.name}</h1>
+
+          .map((fruit) => (
+            <div className="border border-neutral-300 border-solid rounded-lg p-10">
+              <p>
+                <img src={fruit.image} className="w-60 h-60" alt={fruit.name} />
+              </p>
+              <p className="text-3xl font-bold mt-10">{fruit.name}</p>
+              <p className="text-2xl mt-3">{fruit.price} €</p>
+              <div className="mt-3 text-gray-500">
+                <div>calorie: {fruit.nutritions.calories}</div>
               </div>
-              <div
-                className="w-full h-[150px] bg-contain bg-center bg-no-repeat overflow-hidden"
-                style={{ backgroundImage: "url(" + fruit.image + ")" }}
-              ></div>
-              <div className="mt-4 flex items-center justify-center flex-wrap">
-                <p className="text-sm mb-3 mx-2 bg-slate-300 p-1 rounded-md mr-2">
-                  Carboidrati: {fruit.nutritions.carbohydrates}
-                </p>
-                <p className="text-sm mb-3 mx-2 bg-slate-300 p-1 rounded-md mr-2">
-                  Calorie: {fruit.nutritions.calories}
-                </p>
-                <p className=" text-sm mb-3 mx-2 bg-slate-300 p-1 rounded-md mr-2">
-                  Grassi: {fruit.nutritions.fat}%
-                </p>
-                <p className="text-sm mb-3 mx-2 bg-slate-300 p-1 rounded-md mr-2">
-                  Prezzo:{fruit.price}
-                </p>
-              </div>
-              <div className="mt-4 flex justify-center items-center">
-                <button
-                  onClick={() =>
-                    dispatch(
-                      addItem({
-                        ...fruit,
-                        cartId: nanoid(),
-                      })
-                    )
-                  }
-                  className="p-2 bg-green-700 ml-4 rounded-lg text-white"
-                >
-                  Acquista
-                </button>
-                <button
-                  onClick={() => {
-                    setPopUpData(fruit);
-                    handlePopUp();
-                  }}
-                  className="p-2 bg-orange-700 ml-4 rounded-lg text-white"
-                >
-                  Dettagli
-                </button>
-              </div>
+              <button
+                onClick={() =>
+                  dispatch(
+                    addItem({
+                      ...fruit,
+                      cartId: nanoid(),
+                    })
+                  )
+                }
+                className="bg-lime-500 rounded-lg p-3 text-white w-[100%] mt-5"
+              >
+                Acquista
+              </button>
+              <button
+                onClick={() => {
+                  setPopUpData(fruit);
+                  handlePopUp();
+                }}
+                className="bg-orange-400 rounded-lg p-3 text-white w-[100%] mt-3"
+              >
+                Dettagli
+              </button>
             </div>
           ))}
         {popUp && <PopUpFruits fruit={popUpData} state={setPopUp} />}
       </div>
-    </div>
+    </>
   );
 };
 
