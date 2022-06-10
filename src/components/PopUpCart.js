f3087f48328df7c173a53d75085d57ca2a252811
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-06-10 16:57:54 +0200

    fruit-shop

--- src/components/PopUpCart.js
+++ src/components/PopUpCart.js
@@ -1,43 +1,44 @@
 import { useSelector } from "react-redux";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { removeItem } from "../states/cartSlice";
+import { removeItem, totalPrice } from "../states/cartSlice";
 import { useDispatch } from "react-redux";
+import { amountState } from "../states/cartSlice";
 
 const PopUpCart = () => {
   const modalCart = useSelector((state) => state.cart);
-  // console.log(modalCart);
+  const amountFruits = useSelector(amountState);
+  const total = useSelector(totalPrice);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <div className="fixed p-4 max-w-md  rounded-lg border shadow-md sm:p-8 bg-green-500 bg-border-white w-screen h-scrren top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-        <div className="flex justify-between items-center mb-4">
-          <h5 className="text-xl font-bold leading-none text-white pb-3">
-            I tuoi prodotti nel carrello...
-          </h5>
-        </div>
-        {modalCart.fruits.map((fruit, index) => {
-          return (
-            <div
-              key={index}
-              className="flex justify-around items-center text-center text-white text-lg"
-            >
-              <div className="w-[33%]">
-                <img
-                  className="w-[50px] text-center mt-2"
-                  src={fruit.image}
-                  alt={fruit.name}
-                />
-              </div>
-              <div className="w-[33%]">{fruit.name}</div>
-              <div className="w-[33%]">{fruit.price}€</div>
-              <div>
-                <DeleteIcon onClick={() => dispatch(removeItem(fruit))} />
-              </div>
-            </div>
-          );
-        })}
+    <div className="backdrop-blur-md fixed p-4 max-w-md rounded-lg border shadow-md sm:p-8 bg-green-500 bg-border-white w-screen h-scrren top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+      <div className="flex justify-between items-center mb-4">
+        <h5 className="text-xl font-bold leading-none text-white pb-3">
+          {amountFruits <= 0 ? "Il carrello è vuoto" : "Prodotti nel carrello"}
+        </h5>
       </div>
+      {modalCart.fruits.map((fruit, index) => {
+        return (
+          <div
+            key={index}
+            className=" flex justify-around items-center text-center text-white text-lg"
+          >
+            <div className="w-[33%]">
+              <img
+                className="rounded-[100%] h-[50px] w-[50px] text-center mt-2"
+                src={fruit.image}
+                alt={fruit.name}
+              />
+            </div>
+            <div className="w-[33%]">{fruit.name}</div>
+            <div className="w-[33%]">{fruit.price}€</div>
+            <div>
+              <DeleteIcon onClick={() => dispatch(removeItem(fruit))} />
+            </div>
+          </div>
+        );
+      })}
+      <div>Totale:{total}</div>
     </div>
   );
 };
--- src/states/cartSlice.js
+++ src/states/cartSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   fruits: [],
   amount: 0,
+  totalPrice: "",
 };
 
 const cartSlice = createSlice({
@@ -12,6 +13,20 @@ const cartSlice = createSlice({
     addItem: (state, action) => {
       state.fruits = [...state.fruits, action.payload];
       state.amount += 1;
+      if (state.amount > 0) {
+        state.totalPrice = Number(
+          state.fruits
+            .map((price) => {
+              console.log(parseFloat(price.price));
+              const result = parseFloat(price.price.replace(/,/g, ".")).toFixed(
+                2
+              );
+              return parseFloat(result);
+            })
+            .reduce((a, b) => a + b, 0)
+            .toFixed(2)
+        );
+      }
     },
     removeItem: (state, action) => {
       const nextCartFruits = state.fruits.filter(
@@ -26,9 +41,9 @@ const cartSlice = createSlice({
   },
 });
 
-console.log(cartSlice);
-
 export const { addItem, removeItem } = cartSlice.actions;
+export const amountState = (state) => state.cart.amount;
+export const totalPrice = (state) => state.cart.totalPrice;
 
 const cartReducer = cartSlice.reducer;
 export default cartReducer;
