57e984e25f89cc48828ceba6a60e6a5c7343bac7
ameted83 <tedeschiamelia@gmail.com>
2022-07-13 14:24:12 +0200

    updated node.txt

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
+  // async function fetchPlanets() {
+  //   const response = await fetch("http://localhost:3000/planets");
+  //   const planets = await response.json();
+
+  //   console.log({ planets });
+  // }
+
+  // fetchPlanets();
   return (
     <div className="App">
       <Router>
